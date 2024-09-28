2a2b3c6f0700289722d6bc231213009dc8603563
Sabrina Estrada <estrasab@oregonstate.edu>
2024-09-27 19:50:03 -0700

    Implemented a function to track completed training by fiscal year with proper sorting of names.

--- src/index.js
+++ src/index.js
@@ -61,21 +61,66 @@ async function completeledTrainingCount(trainingData) {
         await fsp.writeFile(outputFilePath, JSON.stringify(outputData, null, 2), 'utf8');
 
     } catch (err) {
-        console.error('Error writing the completed_training_count file:', err);
+        console.error('Error writing to the completed_training_count file:', err);
     }
 
     return outputData;
 
 }
 
+async function completedTrainingByFiscalYear(trainingData, trainings, fiscalYear) {
+    const startDate = new Date(`07/01/${fiscalYear - 1}`);
+    const endDate = new Date(`06/30/${fiscalYear}`);
+
+    const trainingCompletions = {
+        trainings: []
+    };
+    
+    trainings.forEach(trainingName => {
+        const completedBy = []; 
+    
+        trainingData.forEach(employee => {
+            const hasCompleted = employee.completions.some(completion => 
+                completion.name === trainingName && 
+                new Date(completion.timestamp) >= startDate && new Date(completion.timestamp) <= endDate
+            );
+    
+            if (hasCompleted) {
+                completedBy.push(employee.name);
+            }
+        });
+    
+        trainingCompletions.trainings.push({
+            name: trainingName,
+            completedBy: completedBy.sort((a, b) => 
+                a.split(' ')[1].localeCompare(b.split(' ')[1]) || 
+                a.split(' ')[0].localeCompare(b.split(' ')[0])
+            )
+        });
+    });
+
+    const outputFilePath = path.join(__dirname, `../output/training_completions_fy_${fiscalYear}.json`);
+
+    try {
+        await fsp.writeFile(outputFilePath, JSON.stringify(trainingCompletions, null, 2), 'utf8');
+    } catch (err) {
+        console.error(`Error writing to the training_completions_fy_${fiscalYear} file:`, err);
+    }
+
+    return trainingCompletions;
+}
+
 async function main() {
     const fileName = path.join(__dirname, '../trainings.txt');
+    const fiscalYear = 2024
+    const trainings = ["Electrical Safety for Labs", "X-Ray Safety", "Laboratory Safety Training"];
 
     try {
         const trainingData = await readTrainingData(fileName);
         const processedTrainingData = await processTrainingData(trainingData);
 
         await completeledTrainingCount(processedTrainingData);
+        await completedTrainingByFiscalYear(processedTrainingData, trainings, fiscalYear);
 
     } catch(err) {
         console.error(err)
